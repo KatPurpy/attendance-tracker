0121843260ef8f6557a6111ae021704e5423cbd7
Kat Purpy <katpurpy@yahoo.com>
2024-03-10 17:34:30 +0300

    starting to work on login page

---
+++ Backend/AttendanceTracker/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using AttendanceTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttendanceTracker.Controllers
+{
+	public class LoginController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult<LoginModel> SignUp([FromForm] LoginModel login)
+		{
+			Console.WriteLine("AAA");
+			var a = Request.Form;
+			return login;
+		}
+
+        [HttpPost]
+        public ActionResult<LoginModel> Login([FromForm] LoginModel model)
+        {
+            return model;
+        }
+    }
+}
---
+++ Backend/AttendanceTracker/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AttendanceTracker.Models
+{
+	public class LoginModel
+	{
+		[EmailAddress]
+		[MaxLength(255)]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+	}
+}
